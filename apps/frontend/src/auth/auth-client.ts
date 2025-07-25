04a1d2d1fe36a5d003ab7b26980fe640970890e1
zoasr <44146518+ZOASR@users.noreply.github.com>
2025-07-26 00:39:09 +0300

    fix(auth): fixed  auth client configuration not sending correct payload

--- apps/frontend/src/auth/auth-client.ts
+++ apps/frontend/src/auth/auth-client.ts
@@ -2,10 +2,7 @@ import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
 	baseURL: import.meta.env.VITE_API_BASE_URL,
-	fetchOptions: { 
+	fetchOptions: {
 		credentials: "include",
-		headers: {
-			"Content-Type": "application/json",
-		},
 	},
 });

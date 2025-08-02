7c7f54ced5cbda04196bf4f7fc0d9a62c7f26e9d
zoasr <44146518+ZOASR@users.noreply.github.com>
2025-08-02 03:25:10 +0300

    fix: fixed type import from backend not working in monorepo

--- apps/frontend/src/lib/api.ts
+++ apps/frontend/src/lib/api.ts
@@ -1,6 +1,68 @@
-import type { SearchResult } from '../../../backend/src/index';
+export type ProductionCompany = {
+	name?: string;
+	id?: number;
+	logo_path?: string;
+	origin_country?: string;
+};
+export type SimplePerson = {
+	id?: number;
+	credit_id?: string;
+	name?: string;
+	gender?: number;
+	profile_path?: string;
+};
+export type Genre = {
+	id?: number;
+	name?: string;
+};
 
-export type { SearchResult } from '../../../backend/src/index';
+export type MovieResult = {
+	poster_path?: string;
+	adult?: boolean;
+	overview?: string;
+	release_date?: string;
+	genre_ids?: Array<number>;
+	id?: number;
+	media_type: 'movie';
+	original_title?: string;
+	original_language?: string;
+	title?: string;
+	backdrop_path?: string;
+	popularity?: number;
+	vote_count?: number;
+	video?: boolean;
+	vote_average?: number;
+};
+export type TvResult = {
+	poster_path?: string;
+	popularity?: number;
+	id?: number;
+	overview?: string;
+	backdrop_path?: string;
+	vote_average?: number;
+	media_type: 'tv';
+	first_air_date?: string;
+	origin_country?: Array<string>;
+	genre_ids?: Array<number>;
+	original_language?: string;
+	vote_count?: number;
+	name?: string;
+	original_name?: string;
+};
+export type SearchResult = (MovieResult | TvResult) & {
+	backdrop: string;
+	director?: string;
+	runtime?: number; // in minutes
+	budget?: number;
+	status?: string;
+	production_countries?: ProductionCompany[];
+	genres?: Genre[];
+	number_of_seasons?: number;
+	number_of_episodes?: number;
+	episode_run_time?: number[];
+	created_by?: SimplePerson[];
+};
+
 export interface SignUpData {
 	name: string;
 	email: string;
